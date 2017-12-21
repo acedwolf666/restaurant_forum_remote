da29201cab76d51ef37fc7182979db288d4e6de2
Ace <waynn7788@gmail.com>
2017-12-21 20:01:42 +0800

    add delete in category index

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,7 +4,11 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
-    @category = Category.new
+    if params[:id]
+      @category = Category.find(params[:id])
+    else
+      @category = Category.new
+    end
   end
 
   def create
@@ -16,6 +20,23 @@ class Admin::CategoriesController < ApplicationController
       @categories = Category.all
       render :index
     end
+  end
+  def update
+    @category = Category.find(params[id])
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+      flash[:notice] = "Category was successfully updated"
+    else
+      @categories = Category.all
+      render :index
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:alert] = "Category was successfully deleted"
+    redirect_to admin_categories_path
   end
 private
   def category_params
