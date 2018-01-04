ed093e80ccc19e6d356c199e8cd73a4ff73d4ec4
Ace <waynn7788@gmail.com>
2018-01-04 22:56:31 +0800

    add users edit function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def show
+  end
+
+  def edit
+    redirect_to user_path(@user)
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+private
+  def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_perams
+    params.require(:user).permit(:name, :intro)
   end
 end
