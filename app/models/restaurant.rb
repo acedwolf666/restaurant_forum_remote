852b5832652003db944b14927ba1a60fb99eb758
Ace <waynn7788@gmail.com>
2018-01-10 01:54:27 +0800

    add is_favored? method to restaurant model for favorite or unfavorite button

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,7 +5,11 @@ class Restaurant < ApplicationRecord
   belongs_to :category, optional: true
 
   has_many :comments, dependent: :destroy
-  
+
   has_many :favorites, dependent: :destroy
   has_many :favored_users, through: :favorites, source: :user
+
+  def is_favored?(user)
+    self.favored_users.include?(user)
+  end
 end
