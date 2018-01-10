a798491958c4d5eabff8da3f4ab962069f8343ca
Ace <waynn7788@gmail.com>
2018-01-10 19:11:33 +0800

    set up routes for like

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,11 @@ Rails.application.routes.draw do
       post :favorite
       post :unfavorite
     end
+
+    member do
+      post :like
+      post :unlike
+    end
   end
 
   resources :categories, only: :show
