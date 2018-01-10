3a973475b6ce7186c4a85bdebb48b8210b2591aa
Ace <waynn7788@gmail.com>
2018-01-11 00:03:07 +0800

    finish like function and dry it with a partial page

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,11 +9,14 @@ class Restaurant < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favored_users, through: :favorites, source: :user
 
-  has_manhy :likes, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
   def is_favored?(user)
     self.favored_users.include?(user)
   end
-
+  
+  def is_liked?(user)
+    self.liked_users.include?(user)
+  end
 end
