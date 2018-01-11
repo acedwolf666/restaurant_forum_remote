5a68cb0c7d0eb243b7029e79f5d15a0e73573c82
Ace <waynn7788@gmail.com>
2018-01-11 19:02:58 +0800

    add count_favorites method to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -15,8 +15,13 @@ class Restaurant < ApplicationRecord
   def is_favored?(user)
     self.favored_users.include?(user)
   end
-  
+
   def is_liked?(user)
     self.liked_users.include?(user)
+  end
+
+  def count_favorites
+    self.favorites_count = self.favorites.size
+    self.save
   end
 end
