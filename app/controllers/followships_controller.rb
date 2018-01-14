777407a37bc8cf98070dfc259b9f8e140705a09c
Ace <waynn7788@gmail.com>
2018-01-14 21:56:02 +0800

    add validation of followed_id in user scope and action for followship

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,2 +1,13 @@
 class FollowshipsController < ApplicationController
+  def create
+  @followship = current_user.followships.new(followed_id: params[:followed_id])
+
+  if @followship.save
+    flash[:notice] = "Successfully followed"
+    redirect_back(fallback_location: root_path)
+  else
+    flash[:alert] = @followship.errors.full_messages.to_sentence
+    redirect_back(fallback_location: root_path)
+  end
+end
 end
--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,4 +1,6 @@
 class Followship < ApplicationRecord
+  validates :followed_id, uniqueness: { scope: :user_id }
+
   belongs_to :user
   belongs_to :followed, class_name: "User"
 end
