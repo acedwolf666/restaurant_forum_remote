b72523ea7e1fd387dc0d87b81cdb86d64c412707
Ace <waynn7788@gmail.com>
2018-01-20 03:10:58 +0800

    add favorites_count,value default in restaurants to make project work on heroku

---
+++ db/migrate/20180119190125_change_favorite_count_in_restaurant.rb
@@ -0,0 +1,5 @@
+class ChangeFavoriteCountInRestaurant < ActiveRecord::Migration[5.1]
+  def change
+    change_column(:restaurants, :favorites_count, :integer, :default => 0, :null=> false)
+  end
+end
